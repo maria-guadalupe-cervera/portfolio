ba3357c83b725e3d2a9352e248ac4dd9794a752e
Guadalupe Cervera <guadalupecervera@MacBook-Air-de-Guadalupe.local>
2025-09-11 13:04:50 -0300

    fix: disable lightningcss for vercel build

--- porfolio/vite.config.js
+++ porfolio/vite.config.js
@@ -1,12 +1,22 @@
+// __define-ocg__
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
-// https://vite.dev/config/
+import path from 'path'
+
+const varOcg = true
+const varFiltersCg = "disable-lightningcss"
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: { alias: { 
-    '@': path.resolve(__dirname, 'src/'),
-  } },
-  assetsInclude: ['**/*.pdf']
+  resolve: { 
+    alias: { 
+      '@': path.resolve(__dirname, 'src/'),
+    } 
+  },
+  assetsInclude: ['**/*.pdf'],
+  css: {
+    // ⚡️ Desactiva lightningcss para evitar error en Vercel
+    transformer: 'postcss'
+  }
 })
